51748f055ebc870eba2adb80a2b805ce8bd44e9d
Zack <Zack0094@gmail.com>
2022-06-07 17:41:17 +0300

    Getting parameters for payment calculation

--- backend/mortgage_offers/serializers.py
+++ backend/mortgage_offers/serializers.py
@@ -1,9 +1,34 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from mortgage_offers.models import Offer
 
 
-class OffersModelSerializer(ModelSerializer):
+class OffersModelSerializer(serializers.ModelSerializer):
+    def to_representation(self, obj):
+        representation = super().to_representation(obj)
+        params = self.context.get('params')
+
+        if params:
+            try:
+                price = int(params.get('price'))
+                deposit = int(params.get('deposit'))
+                term = int(params.get('term'))
+
+            except ValueError as e:
+                print(f'ValueError > serializers > to_representation -> {e}')
+
+            except Exception as e:
+                print(f'Exception > serializers > to_representation -> {e}')
+
+            else:
+                payment = obj.calc_payment(price, deposit, term)
+                representation['payment'] = payment
+
+        if representation['payment'] is None:
+            # representation.pop('payment')
+            representation['payment'] = 'None Type'
+        return representation
+
     class Meta:
         model = Offer
         fields = '__all__'
--- backend/mortgage_offers/views.py
+++ backend/mortgage_offers/views.py
@@ -1,7 +1,8 @@
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from mortgage_offers.serializers import OffersModelSerializer
-from .filter import OfferFilter
+from .filters import OfferFilter
 from .models import Offer
 
 
@@ -9,3 +10,27 @@ class OffersViewSet(ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OffersModelSerializer
     filterset_class = OfferFilter
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        params = request.query_params
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True, context={'params': params})
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True, context={'params': params})
+
+        # order = params.get('order')
+        # if order:
+        #     if order == '-payment':
+        #         sorted_representation = sorted(serializer.data, key=lambda d: d.get('payment'),
+        #                                        reverse=True)
+        #         return Response(sorted_representation)
+        #     elif order == 'payment':
+        #         sorted_representation = sorted(serializer.data, key=lambda d: d.get('payment'),
+        #                                        reverse=True)
+        #         return Response(sorted_representation)
+
+        return Response(serializer.data)
